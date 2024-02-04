1a46ff45145894828c1768962ee5f593301c2650
LiPaVi <lipalacheva@gmail.com>
2024-02-04 22:32:54 +0200

    fix: add cases

--- tests/test_example.py
+++ tests/test_example.py
@@ -2,16 +2,19 @@ import allure
 import requests
 
 
+@allure.id("334")
 @allure.title("Test Example")
 def test_example():
     assert 1 == 1
 
 
+@allure.id("333")
 @allure.title("Test Bad")
 def test_fail():
     assert 1 == 0
 
 
+@allure.id("331")
 @allure.title("Test Error")
 def test_error():
     assert 1 / 0
